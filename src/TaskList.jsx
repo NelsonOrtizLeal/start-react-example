f4a25ec2f37b33a2f6b4f50519b6509eaf3ae1aa
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2023-01-06 17:05:24 -0600

    Modified TaskList.jsc add Taskcard component

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,14 +1,14 @@
 // import { useState, useEffect } from "react";
-
 // console.log(data);
 
-function TaskList({tasks}) {
-//   const [tasks, setTasks] = useState(data);
+import TaskCard from "./TaskCard";
 
-//   useEffect(() => {
-//     setTasks(data)
-//   }, [])
-  
+function TaskList({ tasks }) {
+  //   const [tasks, setTasks] = useState(data);
+
+  //   useEffect(() => {
+  //     setTasks(data)
+  //   }, [])
 
   if (tasks.length === 0) {
     return <div>No hay tareas</div>;
@@ -17,10 +17,7 @@ function TaskList({tasks}) {
   return (
     <div>
       {tasks.map((task, index) => (
-        <div key={index}>
-          <h1>{task.title}</h1>
-          <p>{task.description}</p>
-        </div>
+        <TaskCard task={task} key={index}></TaskCard>
       ))}
     </div>
   );
