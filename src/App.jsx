c31171a4a388b67d392132e04df83459d96ba37f
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2023-01-06 16:37:45 -0600

    Modified App add useState, useEffect and TaskForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "./App.css";
 import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle, taskDescription) {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
+        id: tasks.length + 1,
+        description: taskDescription,
+      },
+    ]);
+  }
+
   return (
-    <div>
-      <TaskList />
-    </div>
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} />
+    </>
   );
 }
 
