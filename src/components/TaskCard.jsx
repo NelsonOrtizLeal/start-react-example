61ba020a1d8c819de331f56fd54c637c2f59e90c
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2023-01-06 18:01:13 -0600

    Modified TaskCard.jsx use deleteTask function

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function TaskCard({task}) {
+function TaskCard({task, deleteTask}) {
+
+  // function deleteTask(id){
+  //   console.log("Eliminando...", id);
+  // }
+
   return (
     <div>
       <h1>{task.title}</h1>
       <p>{task.description}</p>
+      <button onClick={() => {
+        deleteTask(task.id)
+      }}>Eliminar tarea</button>
     </div>
   );
 }
