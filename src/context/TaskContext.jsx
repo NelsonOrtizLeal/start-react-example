9ee56a9fe6c683baf9661011781e4c4add1e104b
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2023-01-06 22:54:06 -0600

    Modified TaskContex add metods and useState and DB

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,11 +1,40 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
 
 // Generar el objeto Context -> Se guarda todas nuestras variables y funciones -> Se debe espicificar
 export const TaskContext = createContext(); // Este es el que almacena los datos
 
 export function TaskContextProvider(props) {
-  let x = 20;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle, taskDescription) {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
+        id: tasks.length + 1,
+        description: taskDescription,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
   return (
-    <TaskContext.Provider value={x}>{props.children}</TaskContext.Provider>
+    <TaskContext.Provider
+      value={{
+        tasks: tasks,
+        createTask: createTask,
+        deleteTask: deleteTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
   );
 }
